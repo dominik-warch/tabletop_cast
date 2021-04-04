b5519098d997d5053bb792cfc8dff263822cfb0f
Dominik <d.visca@posteo.de>
2021-04-04 12:23:03 +0200

    Hooks für Play und Stop

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,23 @@ import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+
+let Hooks = {}
+Hooks.AudioControl = {
+    mounted() {
+        this.handleEvent("play_audio", (payload) => {
+            let audio = document.getElementById(payload.audio_id)
+            audio.play()
+        })
+        this.handleEvent("stop_audio", (payload) => {
+            let audio = document.getElementById(payload.audio_id)
+            audio.pause()
+            audio.currentTime = 0
+        })
+    }
+}
+
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
 
 // Show progress bar on live navigation and form submits
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
